4bece276a00b0a7dce14f357788f06fdfb1ca4af
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-10-20 21:17:54 +0300

    Update SmtpServerConfiguration.cs

--- src/Zetian/Configuration/SmtpServerConfiguration.cs
+++ src/Zetian/Configuration/SmtpServerConfiguration.cs
@@ -24,7 +24,7 @@ namespace Zetian.Configuration
             MaxMessageSize = 10 * 1024 * 1024; // 10 MB
             MaxRecipients = 100;
             MaxConnections = 100;
-            MaxConnectionsPerIp = 10;
+            MaxConnectionsPerIp = 1000;
             ConnectionTimeout = TimeSpan.FromMinutes(5);
             CommandTimeout = TimeSpan.FromMinutes(1);
             DataTimeout = TimeSpan.FromMinutes(3);
